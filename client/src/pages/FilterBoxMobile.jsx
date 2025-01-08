a9f5b3eeb1b2ab31a1881852da4c9e91533e2d1a
fiqrioemry <foemry@gmail.com>
2025-01-08 22:32:12 +0700

    update changes

--- client/src/pages/FilterBoxMobile.jsx
+++ client/src/pages/FilterBoxMobile.jsx
@@ -2,7 +2,28 @@ import { Button } from "@/components/ui/Button";
 import { SlidersHorizontal, SortAsc } from "lucide-react";
 import { DrawerContainer } from "../components/drawer/DrawerContainer";
 
-const FilterBoxMobile = () => {
+const sortValue = [
+  {
+    id: "price",
+    name: "price",
+    value: "asc",
+    label: "minimum price",
+  },
+  {
+    id: "price",
+    name: "price",
+    value: "desc",
+    label: "maximum price",
+  },
+  {
+    id: "price",
+    name: "price",
+    value: "asc",
+    label: "minimum price",
+  },
+];
+
+const FilterBoxMobile = ({ formData }) => {
   return (
     <div className="md:hidden block">
       <div className="flex justify-end py-6">
@@ -11,8 +32,33 @@ const FilterBoxMobile = () => {
             <SlidersHorizontal />
           </Button>
           <DrawerContainer title={<SortAsc />}>
-            <div className="flex items-center justify-center h-[250px]">
-              <h1>area filter</h1>
+            <div className="space-y-6 p-4">
+              <div className="flex items-center justify-between">
+                <label htmlFor="minPrice">Minimum price</label>
+                <input
+                  id="minPrice"
+                  className="rounded-full w-6 h-6"
+                  type="radio"
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="minPrice">Minimum price</label>
+                <input
+                  id="minPrice"
+                  className="rounded-full w-6 h-6"
+                  checked={false}
+                  type="radio"
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="minPrice">Minimum price</label>
+                <input
+                  id="minPrice"
+                  className="rounded-full w-6 h-6"
+                  checked={false}
+                  type="radio"
+                />
+              </div>
             </div>
           </DrawerContainer>
         </div>
