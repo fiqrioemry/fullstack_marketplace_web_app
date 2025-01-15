4ad2c3561a85c65237ea204af282ec40e35f1c91
fiqrioemry <foemry@gmail.com>
2025-01-15 23:58:45 +0700

    update changes

---
+++ server/utils/removeLocalFile.js
@@ -0,0 +1,17 @@
+const fs = require("fs").promises;
+
+/**
+ * Menghapus file lokal berdasarkan path yang diberikan.
+ * @param {string} filePath - Path file yang akan dihapus.
+ * @returns {Promise<void>}
+ */
+async function deleteLocalFile(filePath) {
+  try {
+    await fs.unlink(filePath);
+  } catch (error) {
+    console.error("Failed to delete file:", error);
+    throw new Error("Failed to delete local file");
+  }
+}
+
+module.exports = { deleteLocalFile };
