a402c000420202a02251333cedf406accda38a47
fiqrioemry <foemry@gmail.com>
2025-01-18 16:50:53 +0700

    update changes

--- client/src/pages/seller/Products.jsx
+++ client/src/pages/seller/Products.jsx
@@ -1,7 +1,18 @@
+import { Plus } from "lucide-react";
+import { Button } from "../../components/ui/button";
+
 const Products = () => {
   return (
-    <section className="h-screen flex items-center justify-center">
-      <h1>seller order</h1>
+    <section className="h-screen">
+      <div className="py-6 flex justify-end items-center">
+        <Button>
+          <Plus size={24} />
+          <span className="capitalize">new product</span>
+        </Button>
+      </div>
+      <div className="grid grid-cols-12 gap-2">
+        <div></div>
+      </div>
     </section>
   );
 };
