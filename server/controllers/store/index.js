42d4aa2acb7d1c1d86637e7294ebc66b4f079778
fiqrioemry <foemry@gmail.com>
2025-01-18 21:42:52 +0700

    update changes

--- server/controllers/store/index.js
+++ server/controllers/store/index.js
@@ -1,7 +1,13 @@
 const createSlug = require("../../utils/createSlug");
 const { removeUploadFile } = require("../../utils/removeUploadFile");
 const { uploadMediaToCloudinary } = require("../../utils/cloudinary");
-const { Product, Galleries, Store, sequelize } = require("../../models");
+const {
+  Product,
+  Galleries,
+  Categories,
+  Store,
+  sequelize,
+} = require("../../models");
 
 async function getStoreInfo(req, res) {
   const slug = req.params;
@@ -32,25 +38,64 @@ async function getStoreInfo(req, res) {
 }
 
 async function getAllStoreProducts(req, res) {
-  const slug = req.params;
+  const storeId = req.user.storeId;
+  const { limit, sortBy, order, page, search, category, minPrice, maxPrice } =
+    req.query;
 
   try {
-    const store = await Store.findOne({
-      where: { slug },
-      attributes: ["userId", "name"],
-    });
+    let query = {};
+    if (search) {
+      query[Op.or] = [
+        { name: { [Op.like]: `%${search}%` } },
+        { description: { [Op.like]: `${search}` } },
+      ];
+    }
 
-    if (!store) return res.status(404).json({ message: "Store not found" });
+    if (category) {
+      const findCategory = await Categories.findOne({
+        where: { slug: category },
+      });
+      if (findCategory) {
+        query.categoryId = findCategory.id;
+      } else {
+        return res
+          .status(404)
+          .send({ message: "Product with specified category Not Found" });
+      }
+    }
 
-    const product = await Product.findAll({
-      where: { storeId: store.id },
+    if (minPrice && maxPrice) {
+      query.price = {
+        [Op.between]: [minPrice, maxPrice],
+      };
+    } else if (minPrice) {
+      query.price = {
+        [Op.gte]: minPrice,
+      };
+    } else if (maxPrice) {
+      query.price = {
+        [Op.lte]: maxPrice,
+      };
+    }
+
+    const store = await Store.findByPk(storeId, {
+      where: query,
       include: [
         {
           model: Product,
           as: "product",
-          include: { model: Galleries, as: "galleries" },
+          include: [
+            { model: Galleries, as: "galleries" },
+            {
+              model: Categories,
+              as: "categories",
+              attributes: ["id", "name", "image", "slug"],
+            },
+          ],
         },
       ],
+      distinct: true,
+      order: [[sortBy || "createdAt", order || "DESC"]],
     });
 
     if (!product)
