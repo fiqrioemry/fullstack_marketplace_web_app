225b803baa7ae09122963a22c087835a131fe95c
fiqrioemry <foemry@gmail.com>
2025-01-18 17:17:58 +0700

    update changes

--- client/src/pages/seller/Products.jsx
+++ client/src/pages/seller/Products.jsx
@@ -1,91 +1,57 @@
 import { Edit, Plus, Trash } from "lucide-react";
 import { Button } from "../../components/ui/button";
+import { Input } from "../../components/ui/input";
 
 const Products = () => {
   return (
-    <section className="h-screen">
-      <div className="py-6 flex justify-end items-center">
-        <Button>
+    <section>
+      <div className="py-6 flex justify-between items-center">
+        <div>
+          <Input className="w-96" placeholder="search product" />
+        </div>
+        <Button size="icon md:primary" className="px-2 py-2">
           <Plus size={24} />
-          <span className="capitalize">new product</span>
+          <span className="capitalize md:block hidden">new product</span>
         </Button>
       </div>
       <div>
-        <div className="grid grid-cols-12 gap-2 py-2 border-b border-muted text-sm">
+        <div className="grid grid-cols-12 gap-2 py-2 border-b border-muted text-xs md:text-sm capitalize">
           <div className="col-span-1">no.</div>
           <div className="col-span-2">name</div>
           <div className="col-span-2">preview</div>
           <div className="col-span-2">price</div>
           <div className="col-span-1">stock</div>
           <div className="col-span-2">category</div>
-          <div className="col-span-2">action</div>
+          <div className="col-span-2 flex justify-center">action</div>
         </div>
-        <div className="grid grid-cols-12 gap-2 py-2 border-b border-muted text-sm">
-          <div className="col-span-1">1</div>
-          <div className="col-span-2">Samsung Galaxy</div>
-          <div className="col-span-2">
-            <img
-              className="h-20 w-20 object-contain"
-              src="https://placehold.co/400x400"
-              alt="product"
-            />
+
+        {[...Array(8)].map((_, index) => (
+          <div
+            className="grid grid-cols-12 gap-2 py-2 border-b border-muted text-xs md:text-sm capitalize"
+            key={index}
+          >
+            <div className="col-span-1">{index + 1}</div>
+            <div className="col-span-2">Samsung Galaxy</div>
+            <div className="col-span-2">
+              <img
+                className="h-20 w-20 object-contain"
+                src="https://placehold.co/400x400"
+                alt="product"
+              />
+            </div>
+            <div className="col-span-2">Rp. 50.000</div>
+            <div className="col-span-1">50</div>
+            <div className="col-span-2">Electronic</div>
+            <div className="col-span-2 flex justify-center space-x-2">
+              <Button size="icon">
+                <Edit />
+              </Button>
+              <Button size="icon" variant="destructive">
+                <Trash />
+              </Button>
+            </div>
           </div>
-          <div className="col-span-2">Rp. 50.000</div>
-          <div className="col-span-1">50</div>
-          <div className="col-span-2">Electronic</div>
-          <div className="col-span-2 space-x-2">
-            <Button>
-              <Edit />
-            </Button>
-            <Button variant="destructive">
-              <Trash />
-            </Button>
-          </div>
-        </div>
-        <div className="grid grid-cols-12 gap-2 py-2 border-b border-muted text-sm">
-          <div className="col-span-1">1</div>
-          <div className="col-span-2">Samsung Galaxy</div>
-          <div className="col-span-2">
-            <img
-              className="h-20 w-20 object-contain"
-              src="https://placehold.co/400x400"
-              alt="product"
-            />
-          </div>
-          <div className="col-span-2">Rp. 50.000</div>
-          <div className="col-span-1">50</div>
-          <div className="col-span-2">Electronic</div>
-          <div className="col-span-2 space-x-2">
-            <Button>
-              <Edit />
-            </Button>
-            <Button variant="destructive">
-              <Trash />
-            </Button>
-          </div>
-        </div>
-        <div className="grid grid-cols-12 gap-2 py-2 border-b border-muted text-sm">
-          <div className="col-span-1">1</div>
-          <div className="col-span-2">Samsung Galaxy</div>
-          <div className="col-span-2">
-            <img
-              className="h-20 w-20 object-contain"
-              src="https://placehold.co/400x400"
-              alt="product"
-            />
-          </div>
-          <div className="col-span-2">Rp. 50.000</div>
-          <div className="col-span-1">50</div>
-          <div className="col-span-2">Electronic</div>
-          <div className="col-span-2 space-x-2">
-            <Button size="icon">
-              <Edit />
-            </Button>
-            <Button size="icon" variant="destructive">
-              <Trash />
-            </Button>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
