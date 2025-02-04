57621e3718dd83528e48fb936ee50343ad39051d
fiqrioemry <foemry@gmail.com>
2025-02-04 23:15:23 +0700

    update changes

---
+++ client/src/components/auth/StepThree.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+
+import { registerControl } from "@/config";
+import InputForm from "@/components/form/InputForm";
+import InputButton from "@/components/form/InputButton";
+
+const StepThree = ({ registerForm }) => {
+  return (
+    <div>
+      <InputForm formik={registerForm} formControl={registerControl}>
+        <InputButton title={"register"} formik={registerForm} />
+      </InputForm>
+    </div>
+  );
+};
+
+export default StepThree;
