efdd1e8dc2397fde7c951ef4b9b0c459bdeed088
fiqrioemry <foemry@gmail.com>
2025-02-05 21:52:02 +0700

    update changes

---
+++ client/src/hooks/useFormSchema.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useFormik } from "formik";
+import { newValidationSchema } from "@/lib/utils";
+
+export const useFormSchema = (state, control, action, params) => {
+  const formik = useFormik({
+    initialValues: state,
+    validationSchema: newValidationSchema(control),
+    enableReinitialize: true,
+    onSubmit: (values) => {
+      action(values, params);
+    },
+  });
+
+  useEffect(() => {
+    formik.validateForm();
+
+    formik.setTouched({});
+  }, [control]);
+
+  return formik;
+};
