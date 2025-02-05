f480f6f51a6fd33f0ba5c59c3111be93ead84715
fiqrioemry <foemry@gmail.com>
2025-02-05 21:51:32 +0700

    update changes

---
+++ client/src/services/instance.js
@@ -0,0 +1,72 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+export const publicInstance = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    'X-API-Key': import.meta.env.VITE_API_KEY,
+  },
+});
+
+export const authInstance = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    'X-API-Key': import.meta.env.VITE_API_KEY,
+  },
+});
+
+authInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = Cookies.get('accessToken');
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+
+  (error) => {
+    Promise.reject(error);
+  },
+);
+
+authInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (
+      error.response.status === 401 &&
+      error.config &&
+      !error.config.__isRetryRequest
+    ) {
+      try {
+        const response = await axios.post(
+          `${import.meta.env.VITE_BASE_URL}/auth/refresh`,
+          {
+            withCredentials: true,
+            headers: {
+              'Content-Type': 'application/json',
+              'X-API-Key': import.meta.env.VITE_API_KEY,
+            },
+          },
+        );
+
+        const newAccessToken = response.data.accessToken;
+
+        Cookies.set('accessToken', newAccessToken, { expires: 1 / 96 });
+
+        error.config.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        return authInstance(error.config);
+      } catch (refreshError) {
+        Cookies.remove('accessToken');
+
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  },
+);
