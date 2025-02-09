63e99a3381b6a71e92419d958d40a3e4cab32d78
fiqrioemry <foemry@gmail.com>
2025-02-09 21:52:28 +0700

    update changes

---
+++ server/migrations/20250209144918-create-transaction.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      totalPrice: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+        defaultValue: 0,
+      },
+      totalShipmentCost: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+        defaultValue: 0,
+      },
+      amountToPay: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+        defaultValue: 0,
+      },
+      paymentStatus: {
+        type: Sequelize.ENUM('pending', 'paid', 'expired', 'canceled'),
+        allowNull: false,
+        defaultValue: 'pending',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Transactions');
+  },
+};
---
+++ server/models/transaction.js
@@ -0,0 +1,52 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Transaction extends Model {
+    static associate(models) {
+      this.belongsTo(models.User, {
+        foreignKey: 'userId',
+        as: 'user',
+      });
+      this.hasMany(models.Order, {
+        foreignKey: 'transactionId',
+        as: 'orders',
+      });
+    }
+  }
+
+  Transaction.init(
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      totalPrice: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+        defaultValue: 0,
+      },
+      totalShipmentCost: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+        defaultValue: 0,
+      },
+      amountToPay: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+        defaultValue: 0,
+      },
+      paymentStatus: {
+        type: DataTypes.ENUM('pending', 'paid', 'expired', 'canceled'),
+        allowNull: false,
+        defaultValue: 'pending',
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Transaction',
+    },
+  );
+
+  return Transaction;
+};
