dd9eef4586d1ff10f17e974c05c21524d5144e33
fiqrioemry <foemry@gmail.com>
2025-03-10 23:53:25 +0700

    update changes

---
+++ client/src/components/cart/CartLayout.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+const CartLayout = ({ children }) => {
+  return (
+    <section className="bg-gray-100 px-2">
+      <div className="container mx-auto py-3 md:py-6">
+        <h2 className="mb-4">Keranjang</h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">{children} </div>
+      </div>
+    </section>
+  );
+};
+
+export default CartLayout;
--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -3,6 +3,7 @@ import { useCartStore } from "@/store/useCartStore";
 import CartLoading from "@/components/loading/CartLoading";
 import TotalCartPrice from "@/components/cart/TotalCartPrice";
 import CartItemDisplay from "@/components/cart/CartItemDisplay";
+import CartLayout from "../components/cart/CartLayout";
 
 const Cart = () => {
   const { cart, getCarts } = useCartStore();
@@ -11,18 +12,18 @@ const Cart = () => {
     getCarts();
   }, [getCarts]);
 
-  if (!cart) return <CartLoading />;
+  if (!cart)
+    return (
+      <CartLayout>
+        <CartLoading />
+      </CartLayout>
+    );
 
   return (
-    <section className="bg-gray-100 px-2">
-      <div className="container mx-auto py-3 md:py-6">
-        <h2 className="mb-4">Keranjang</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          <CartItemDisplay cart={cart} />
-          <TotalCartPrice cart={cart} />
-        </div>
-      </div>
-    </section>
+    <CartLayout>
+      <CartItemDisplay cart={cart} />
+      <TotalCartPrice cart={cart} />
+    </CartLayout>
   );
 };
 
