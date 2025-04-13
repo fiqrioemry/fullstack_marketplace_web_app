37aeab44d445a8cc171df610be9ebd7138556ef6
Ahmad Fiqri Oemry <121015122+fiqrioemry@users.noreply.github.com>
2025-04-13 22:59:21 +0700

    delete folder

--- server/routes/product/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const { getProduct, getProducts } = require('../../controllers/product');
-
-router.get('/', getProducts);
-router.get('/:slug', getProduct);
-
-module.exports = router;
